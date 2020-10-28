cf9e1560146a563a9030010e34d9401dd9e6c396
Elizaveta <69856251+TheCoolestMango@users.noreply.github.com>
2020-10-28 19:05:56 +0300

    Update ex4.c

--- week11/ex4.c
+++ week11/ex4.c
@@ -10,7 +10,7 @@ int main() {
     int from = open("ex1.txt", O_RDWR);
     int to = open("ex1.memcpy.txt", O_RDWR);
     int sizeOfFrom = lseek(from,0,SEEK_END);
-    ftruncate(destination, sizeOfFrom);
+    ftruncate(to, sizeOfFrom);
     char *mem_from = mmap(NULL, sizeOfFrom, PROT_READ| PROT_WRITE, MAP_SHARED,from, 0);
     char *mem_to = mmap(NULL, sizeOfFrom, PROT_READ| PROT_WRITE, MAP_SHARED,to, 0);
     memcpy(mem_to, mem_from, sizeOfFrom);
